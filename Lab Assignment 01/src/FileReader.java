ff77b8e5e4dfcd0e9098867d391663883f6c1983
gabbley <baniquedg@bishopireton.org>
2017-09-23 21:39:32 -0400

    successful test of p1/2 can't print to output file correctly

--- Lab Assignment 01/src/FileReader.java
+++ Lab Assignment 01/src/FileReader.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class FileReader {
 
 	private static String outputFile = "output.txt";
-	
+
 	public static void main(String[] args) {
 		if (args.length < 2) {
 			System.out.println("Not enough files provided");
@@ -17,15 +17,16 @@ public class FileReader {
 		}
 
 		Scanner in = openFile(args[0]);
-		if (in == null){
+		if (in == null) {
 			System.exit(1);
 		}
 
 		part1(args[0]);
 		part2(args[0], args[1]);
+		part3(openFile(args[2]));
 	}
 
-	public static Scanner openFile(String filename) {
+	public static Scanner openFile(String filename) { //file to Scanner object
 
 		File f = new File(filename);
 		Scanner input = null;
@@ -37,7 +38,8 @@ public class FileReader {
 		return input;
 	}
 
-	public static PrintWriter canBeOpened(String filename, int part) {
+	public static PrintWriter canBeOpened(String filename, int part) { 
+		//returns  a Printwriter if file can be opened/found
 
 		File f = new File(filename);
 		PrintWriter output = null;
@@ -49,91 +51,111 @@ public class FileReader {
 		}
 		return output;
 	}
-	
-	//create a method that returns either a full word or a word within brackets
 
+	// create a method that returns either a full word or a word within brackets
 
 	public static void part1(String file) {
 		PrintWriter output = canBeOpened(file, 1);
 		Scanner p1 = openFile(file);
 		if (checkBraces(p1)) {
-			output.println("Braces Balanced\n");
+			System.out.println("Braces Balanced\n");
 		} else {
 			output.println("Braces Not Balanced\n");
 		}
-		
+
 	}
-	
-	public static void part2(String file1, String file2){
+
+	public static void part2(String file1, String file2) {
 		PrintWriter output = canBeOpened(file2, 2);
 		Scanner p1 = openFile(file1);
 		Scanner p2 = openFile(file2);
-		if (compareFiles(p1, p2)){
-			output.println("Files Identical\n");
-		}
-		else{
+		if (compareFiles(p1, p2)) {
+			System.out.println("Files Identical\n");
+		} else {
 			output.println("Files Not Identical\n");
 		}
 	}
-	
-	public static void part3(Scanner story){
-		ArrayList<String> everyWord = new ArrayList<String>();
+
+	public static void part3(Scanner story) {
+		ArrayList<String> finalWords = new ArrayList<String>();
 		ArrayList<Integer> positions = new ArrayList<Integer>();
-		
-		while (story.hasNext()){
-			int i = 0;
-			everyWord.add(story.next()); //add every word to ArrayList
-			if (story.next().indexOf("<") != -1 && story.next().indexOf("/>") != -1){
-				 positions.add(everyWord.size()-1);
-				 i++;
+
+		while (story.hasNext()) { //while there are more words
+			finalWords = everyWord(story.nextLine());
+		}
+	}
+
+	public static boolean compareFiles(Scanner p1, Scanner p2) {
+		ArrayList<String> str1 = new ArrayList<String>();
+		ArrayList<String> str2 = new ArrayList<String>();
+		boolean result = true;
+
+		while (p1.hasNextLine()) {
+			str1 = everyWord(p1.nextLine());
+		}
+
+		while (p2.hasNextLine()) {
+			str2 = (everyWord(p2.nextLine()));
+		}
+
+		for (int i = 0; i<str1.size(); i++){
+			if (!str1.get(i).equals(str2.get(i))){
+				result = false;;
 			}
 		}
+		
+	return result;
+
 	}
 	
-	public static boolean compareFiles(Scanner p1, Scanner p2){
-		String str1 = "";
-		String str2 = "";
+	public static ArrayList<String> everyWord(String words){
+		ArrayList<String> finalWords = new ArrayList<String>();
+		for (int i = 0; i < words.length(); i++){
+			String s = "";
 		
-		while (p1.hasNextLine()) {
-		str1 += p1.nextLine();	
+			if (words.charAt(i) != ' ' || words.charAt(i) != '<') {
+				s += words.charAt(i);
+			}
+			else{
+				s = words.substring(i, words.indexOf('>'));
+			}
+				
+			finalWords.add(s);
+			//put everything before a space in 
+			}
+		
+		return finalWords;
+			
 		}
-		
-		while (p2.hasNextLine()) {
-		str2 += p1.nextLine();	
-		}
-		
-		return (str1.equals(str2));
-		
-	}
+
+	
 
 	public static boolean checkBraces(Scanner in) {
 		int openBrace = 0;
 		int closedBrace = 0;
-		int countOpen = 0;
-		int countClosed = 0;
+		int check = 0;
 		
-		while(in.hasNextLine()) {
+		while (in.hasNextLine()) {
 			char[] arrChar = in.nextLine().toCharArray();
 			for (char c : arrChar) {
-				if (c== '{') {
-					openBrace++;
-					while (countClosed == 0) {
-					countOpen++;
-					}
+				while (check == 0) {
+					if (c == '{') {
+						openBrace++;
+					} 
 					
-				}
-				else if (c == '}'){
-					closedBrace++;
+					else if (c == '}') {
+						closedBrace++;
+						check++;
+					}
 				}
 			}
-	
+
 		}
-		if (openBrace == closedBrace){
+		if (openBrace == closedBrace) {
 			return true;
-		}
-		else
+		} else
 			return false;
-		
+
 	}
 
 }
